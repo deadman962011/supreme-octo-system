4739ab5fa2c92b813b9c2afda47075de345d0f50
shamelapp2025 <shamelapp2025@gmail.com>
2024-09-08 19:48:01 +0400

    9/8

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 ThemeData dark = ThemeData(
   fontFamily: 'Roboto',
-  primaryColor: const Color(0xFF54b46b),
-  secondaryHeaderColor: const Color(0xFF009f67),
+  primaryColor: const Color.fromRGBO(242, 157, 65, 1),
+  secondaryHeaderColor: const Color.fromARGB(255, 255, 196, 132),
   disabledColor: const Color(0xFF6f7275),
   brightness: Brightness.dark,
   hintColor: const Color(0xFFbebebe),
   cardColor: Colors.black,
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color(0xFF54b46b))),
-  colorScheme: const ColorScheme.dark(primary: Color(0xFF54b46b), secondary: Color(0xFF54b46b)).copyWith(error: const Color(0xFFdd3135)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color.fromRGBO(242, 157, 65, 1))),
+  colorScheme: const ColorScheme.dark(primary: const Color.fromRGBO(242, 157, 65, 1), secondary: const Color.fromRGBO(242, 157, 65, 1)).copyWith(error: const Color(0xFFdd3135)),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white10),
   floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-
+// f29d41
 ThemeData light = ThemeData(
-  fontFamily: 'Roboto',
-  primaryColor: const Color(0xFF2A9849),
-  secondaryHeaderColor: const Color(0xFF1ED7AA),
+  fontFamily: 'Roboto', 
+  primaryColor: const Color.fromRGBO(242, 157, 65, 1),
+  secondaryHeaderColor: const Color.fromARGB(255, 255, 196, 132),
   disabledColor: const Color(0xFFA0A4A8),
   brightness: Brightness.light,
   hintColor: const Color(0xFF9F9F9F),
   cardColor: Colors.white,
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color(0xFF2A9849))),
-  colorScheme: const ColorScheme.light(primary: Color(0xFF2A9849), secondary: Color(0xFF2A9849)).copyWith(error: const Color(0xFFE84D4F)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color.fromRGBO(242, 157, 65, 1))),
+  colorScheme: const ColorScheme.light(primary: Color.fromRGBO(242, 157, 65, 1), secondary: const Color.fromRGBO(242, 157, 65, 1)).copyWith(error: const Color(0xFFE84D4F)),
   popupMenuTheme: const PopupMenuThemeData(color: Colors.white, surfaceTintColor: Colors.white),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white),
   floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
