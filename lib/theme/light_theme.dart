05587c39515217a7cc9b2fd4845af7bf45001dbb
shamelapp2025 <shamelapp2025@gmail.com>
2024-09-11 20:37:38 +0400

    9/11

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 // f29d41
 ThemeData light = ThemeData(
   fontFamily: 'Roboto', 
-  primaryColor: const Color.fromRGBO(242, 157, 65, 1),
-  secondaryHeaderColor: const Color.fromARGB(255, 255, 196, 132),
+  primaryColor: const Color(0xFF560BAD),
+  secondaryHeaderColor: const Color(0xFF7209B7) ,
   disabledColor: const Color(0xFFA0A4A8),
   brightness: Brightness.light,
   hintColor: const Color(0xFF9F9F9F),
   cardColor: Colors.white,
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color.fromRGBO(242, 157, 65, 1))),
-  colorScheme: const ColorScheme.light(primary: Color.fromRGBO(242, 157, 65, 1), secondary: const Color.fromRGBO(242, 157, 65, 1)).copyWith(error: const Color(0xFFE84D4F)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color(0xFF560BAD))),
+  colorScheme: const ColorScheme.light(primary: Color(0xFF560BAD), secondary: const Color(0xFF7209B7)).copyWith(error: const Color(0xFFE84D4F)),
   popupMenuTheme: const PopupMenuThemeData(color: Colors.white, surfaceTintColor: Colors.white),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white),
   floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
